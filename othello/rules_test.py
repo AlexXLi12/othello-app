15be7d758600100425cace5f235b5b0d73aa14aa
AlexXLi12 <alexxuanqili@gmail.com>
2025-04-19 21:21:28 -0700

    Refactor move validation tests to include additional assertions and restore midgame checks

--- othello/rules_test.py
+++ othello/rules_test.py
@@ -12,11 +12,12 @@ def midgame_board():
 def test_check_move_starting(starting_board):
     assert check_move(starting_board, 26, 27, 'X') == 28
     assert check_move(starting_board, 19, 27, 'X') == 35 
+    assert check_move(starting_board, 26, 19, 'O') == -1
 
-# TODO: Fix these tests
-#def test_check_move_midgame(midgame_board):
-    #assert check_move(midgame_board, 26, 27, 'X') == 28
-    #assert check_move(midgame_board, 19, 27, 'X') == 35 
+def test_check_move_midgame(midgame_board):
+    assert check_move(midgame_board, 19, 27, 'X') == 35
+    assert check_move(midgame_board, 34, 35, 'O') == 36 
 
 def test_possible_moves_midgame(midgame_board):
     assert get_possible_moves(midgame_board, 'X') == {38, 46, 51, 19, 21, 53}
+    assert get_possible_moves(midgame_board, 'O') == {26, 34, 41, 49}
